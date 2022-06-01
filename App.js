3b25bb9da8ce7c6ae471d3fb806bc4373ac4bf35
DARKnx <joao.vitornl@gmail.com>
2022-05-31 21:41:42 -0300

    :sparkles: feat: Construção da pagina de login

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar, View,Text} from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes';
+
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const [fontsLoaded] = useFonts({
+
+    'Inter-Black': require('./src/assets/fonts/Inter-Black.ttf'),
+    'Inter-Bold': require('./src/assets/fonts/Inter-Bold.ttf'),
+    'Inter-Medium': require('./src/assets/fonts/Inter-Medium.ttf'),
+
+  })
+
+  if (!fontsLoaded){
+    return <AppLoading />;
+  }
+
+  return (
+
+<View style={{fontFamily: 'Inter-Medium', height: '100%'}}>
+    <StatusBar backgroundColor={"#F6D03C"} barStyle="light-content"></StatusBar>
+    <Routes/>
+    </View>
+
+  );
+
+}
---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+//import  SignUp from '../pages/SignUp';
+import SignIn from '../pages/SignIn';
+
+
+export default function Routes(){
+
+return (<SignIn></SignIn>)
+
+}
