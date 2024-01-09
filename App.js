eb27ef9b7e043179f4f9ada9e2a17689cba6dd93
Dutra PC <joao.vitornl@gmail.com>
2024-01-09 18:53:36 -0300

    :construction: Add expo font loader

--- App.js
+++ App.js
@@ -3,8 +3,19 @@ import { StyleSheet, Text, View } from 'react-native';
 import Router from './src/routes';
 import { ThemeProvider } from 'styled-components';
 import { LightTheme } from './src/styles/themes/light';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+
+  const [loaded] = useFonts({
+    'regular': require('./assets/fonts/Inter-Regular.ttf'),
+    'medium': require('./assets/fonts/Inter-Medium.ttf'),
+    'black': require('./assets/fonts/Inter-Black.ttf'),
+    'light': require('./assets/fonts/Inter-Light.ttf'),
+    'bold': require('./assets/fonts/Inter-Bold.ttf'),
+  });
+
+  if (!loaded) return <View><Text>carregando fonte...</Text></View>
 
   return (
     <ThemeProvider theme={LightTheme}>
